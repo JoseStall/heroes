e8c524d5c3b34b5706ab365ddf3dfa2a7c66df3d
guillaume <g.delory62@gmail.com>
2018-10-22 21:18:18 +0200

    rest_crud

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'IronMan' },
+  { id: 12, name: 'Hulk' },
+  { id: 13, name: 'Ultron' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Spiderman' },
+  { id: 16, name: 'Dr Strange' },
+  { id: 17, name: 'Black Panther' },
+  { id: 18, name: 'Ant-Man' },
+  { id: 19, name: 'DeadPool' },
+  { id: 20, name: 'Groot' }
+];
